f13f062cce20a1649243a58650354873ad750577
Noela-Murugi <murugin254@gmail.com>
2022-08-17 11:28:15 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+//initialises the customerName
+var customerName = 'bob';
+
+//modifies customer name to upper case
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+}
+
+//setBestCustomer function
+function setBestCustomer(){
+    bestCustomer = "not bob";
+
+}
+
+//overwriteBestCustomer function
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+
+}
+
+//attempts to change the constant value
+const leastFavoriteCustomer = "nate"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Noela";
+}
